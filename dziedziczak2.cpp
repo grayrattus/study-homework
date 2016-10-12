56a4638ba98d51bf4511ed92361b437fd05ecd67
grayrattus <arturdziedziczak0@gmail.com>
2016-10-12 15:08:43 +0000

    Update for excersise 2, added comments and solution linear equation

--- dziedziczak2.cpp
+++ dziedziczak2.cpp
@@ -3,72 +3,107 @@
 #include <math.h>
 using namespace std;
 
+// Użyłem tutaj klasy ponieważ chciałem spróbować czegoś nowego a już wczesniej programowałem więc
+//  stwierdziłem, że wykonam program na klasach c++.
+
+// Deklaracja klasy abc, która przechowuje zmienne a,b,c potrzebne do rozwiązania równania.
+// set* -- przypisuje zmienne do klasy
+// get* -- zwraca zmienne klasy
+
 class abc {
-    double a;
-    double b;
-    double c;
+    float a;
+    float b;
+    float c;
 
     public:
-    void seta(int i) { a = i; } 
-    void setb(int i) { b = i; } 
-    void setc(int i) { c = i; } 
+    void setA(float i) { a = i; } 
+    void setB(float i) { b = i; } 
+    void setC(float i) { c = i; } 
+    void setABC(float tempA, float tempB, float tempC) { 
+        a = tempA;
+        b = tempB;
+        c = tempC;
+    } 
 
-    int geta() {return a;  } 
-    int getb() {return b;  } 
-    int getc() {return c;  } 
+    float getA() {return a;  } 
+    float getB() {return b;  } 
+    float getC() {return c;  } 
 
 };
-
-double *rozwiaz(double a, double b, double c, double *x) {
-    double delta;
-
-    delta = b*b-(4*a*c);
-
-    if ( delta > 0 ) {
-       x[0] = ((-b)+delta)/(-2*a); 
-       x[1] = ((-b)-delta)/(-2*a); 
-
-       return x;
-    } else if ( delta == 0 ) {
-       x[0] =  
-    } else {
-    }
-
-}
 
 int main(int argc, char *argv[]){
     cout << "Dziedziczak Artur" << endl;
 
-    double x[2] = {0,0};
-
-
+    // Deklaracja i inicjalizacja tablicy zmiennych typu klasy abc
     abc zmienne[6];
 
-    zmienne[0].seta(3);
-    zmienne[0].setb(6);
-    zmienne[0].setc(3);
-    
-    zmienne[1].seta(2.5);
-    zmienne[1].setb(6.6);
-    zmienne[1].setc(1.5);
-    
-    zmienne[2].seta(0.5);
-    zmienne[2].setb(0.1);
-    zmienne[2].setc(1.5);
-    
-    zmienne[3].seta(0);
-    zmienne[3].setb(2.5);
-    zmienne[3].setc(7.5);
-    
-    zmienne[4].seta(0);
-    zmienne[4].setb(0);
-    zmienne[4].setc(5.5);
-    
-    zmienne[5].seta(0);
-    zmienne[5].setb(0);
-    zmienne[5].setc(0);
+    zmienne[0].setABC(3,6,3);
+    zmienne[1].setABC(2.5,6.6,1.5);
+    zmienne[2].setABC(0.5,0.1,1.5);
+    zmienne[3].setABC(0,2.5,7.5);
+    zmienne[4].setABC(0,0,5.5);
+    zmienne[5].setABC(0,0,0);
 
-    rozwiaz(1,2,3, x);
-    
+    // Deklaracja zmiennnych potrzebnych do rozwiązania równania
+    float delta = 0;
+    float x1, x2 = 0;
+
+    // Zbadanie ilości elementów tablicy poprzez
+    //  zizeof(zmienne) -- zwróci ilość bajtów, które zajmuje cała tablica
+    //  sizeof(abc) -- zwróci ilość bajtów jednego elemetu tablicy
+    //      zmienneSiz -- tutaj przypisuję ilośc elementów bajtyTablicy/bajtyJednegoElementu = iloscElementówWTablicy
+    int zmienneSiz = sizeof(zmienne)/sizeof(abc);
+
+    // Pętla iterująca po elementach tablicy
+    for ( int i = 0 ; i < zmienneSiz ; i++ ) {
+        cout << "\n\n";
+
+        // Wypisuje na ekranie wartości a,b,c 
+        cout << "a: " << zmienne[i].getA() << 
+            "b: " << zmienne[i].getB() << 
+            "c: " << zmienne[i].getC() << endl;
+
+
+        // Sprawdza czy równanie posiada x.
+        //  Jeżeli a = 0 i b = 0 to nie jest to równanie, które można rozwiązać
+        if ( zmienne[i].getA() == 0 && zmienne[i].getB() == 0 ){
+            cout << "Współczynnik a = 0 i b = 0 więc nie jest to równanie" << endl;
+        }
+
+        // Sprawdza czy równanie jest równaniem kwadratowym
+        //  Jeżeli nie jest bo a = 0 to oblicza równanie liniowe ze wzoru y=ax+b
+        else if ( zmienne[i].getA() == 0 ){
+            x1 = (-zmienne[i].getC())/zmienne[i].getB();
+            cout << "Współczynnik a = 0 więc jest to równanie liniowe z rozwiązaniem x = " << x1 << endl;
+        }
+        // W przeciwnych wypadkach algorytm rozwiązuje równanie kwadratowe
+        else {
+
+            // Oblicza delte
+            delta = (zmienne[i].getB()*zmienne[i].getB())-(4*zmienne[i].getA()*zmienne[i].getC());
+
+            // Jezeli delta jest większa > 0 to równanie ma 2 pierwiastki, które program wypisze na ekranie
+            if ( delta > i ) {
+                x1 = ((-zmienne[i].getB())+delta)/(2*zmienne[i].getA()); 
+                x2 = ((-zmienne[i].getB())-delta)/(2*zmienne[i].getA()); 
+
+                cout << "Delta " << delta << " > 0 Równanie ma 2 rozwiązania x1:" << x1 << " x2: " << x2 << endl;
+
+            } 
+            // Jeżeli delta jest == 0 to równanie ma 1 pierwiastek
+            else if ( delta == 0 ) {
+                x1 = (-zmienne[i].getB())/(2*zmienne[i].getA()); 
+
+                cout << "Delta " << delta << " = 0 Równanie ma 1 rozwiązanie x:" << x1 << endl;
+            }
+            // W przeciwnym wypadku wszystkie możliwości zostały wykorzystane i delta musi być < 0
+            //  wtedy równanie nie ma rozwiązania 
+            else {
+                cout << "Delta " << delta << " < 0 Brak rozwiązań " << endl;
+            }
+
+        }
+    }
+
     return 0;
 }
